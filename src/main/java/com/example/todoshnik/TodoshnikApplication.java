f29c50554c82c1789fa8b1d736ce5e538b7dc7bf
glebchik_gg <gaben123001@gmail.com>
2025-05-01 22:12:00 +0300

    first commit

---
+++ src/main/java/com/example/todoshnik/TodoshnikApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoshnik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoshnikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoshnikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/todoshnik/TodoshnikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoshnik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoshnikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
